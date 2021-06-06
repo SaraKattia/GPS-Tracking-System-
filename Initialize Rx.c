5c20a33f8966473b50e1b5c2640997f30c9f3fb1
Salma <85197391+salmaasherif@users.noreply.github.com>
2021-06-06 15:06:32 +0200

    Update Initialize Rx.c

--- Initialize Rx.c
+++ Initialize Rx.c
@@ -1,12 +1,1 @@
-	// not tested yet
-	char* init_Rx()
-        {
-        int i;
-        while(1){
-        while ((UART7_FR_R & 0x0010) != 0 ){};
-        static char x[100];
-        for( i=0; i<100; i++)
-        {
-            x[i] = UART7_DR_R;
-            return x;}
-        }}
+
